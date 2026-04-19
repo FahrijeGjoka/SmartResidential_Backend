761a49da0273edd14b5936987dfeda717e35c865
Florjete Kuka <florjetekuka@gmail.com>
2026-04-19 20:29:55 +0200

    SG1S-23 refactor: standardize issue repositories to extend BaseRepository #done

--- src/main/java/com/smartresidential/backend/repositories/IssueAssignmentRepository.java
+++ src/main/java/com/smartresidential/backend/repositories/IssueAssignmentRepository.java
@@ -1,11 +1,10 @@
 package com.smartresidential.backend.repositories;
 
 import com.smartresidential.backend.entities.IssueAssignment;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface IssueAssignmentRepository extends JpaRepository<IssueAssignment, Long> {
+public interface IssueAssignmentRepository extends BaseRepository<IssueAssignment, Long>  {
 
     List<IssueAssignment> findByIssueId(Long issueId);
 
